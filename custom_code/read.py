37308d07372f915b69e8ae5dc7ac385b54d535b7
aymeric75 <aymeric.barbin@hotmail.fr>
2026-01-26 15:43:04 +0800

    last

--- custom_code/read.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-
-print(pd.read_parquet("mymessages.parquet"))
--- custom_code/test.py
+++
@@ -1,7 +0,0 @@
-
-import numpy as np
-
-retour = np.searchsorted([0.1, 0.11, 0.11, 0.12, 0.12, 4,5], 0.13, side="left")
-
-
-print(retour)
